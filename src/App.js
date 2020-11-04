d44970a7779eb9340d9a13a019298c9a409aa577
Chris <34993144+Mesiow@users.noreply.github.com>
2020-11-04 11:36:10 -0500

    add hacky overflow fix for message list

--- src/App.js
+++ src/App.js
@@ -5,18 +5,26 @@ import {Container} from 'semantic-ui-react';
 import {Grid, Segment} from 'semantic-ui-react';
 import MessageList from './Components/MessageList';
 import SendMessageForm from './Components/SendMessageForm';
-import RoomList from './Components/RoomList';
+import UserList from './Components/UserList';
 import NewRoomForm from './Components/NewRoomForm';
 import {socket} from './Socket';
+
+const gridStyles={
+    backgroundColor:"#0a376b",
+    height:"80vh",
+    borderRadius:"10px",
+};
 
 
 function App() {
   //const [message, setMessage] = useState({name: "", message: ""});
   const [messageList, setMessageList] = useState([]);
+  const [usersConnected, setUsersConnected] = useState([]);
 
   useEffect(() => {
     //add new name and message to message list when we receive a message event
     socket.on("message", ({name, text}) => {
+      console.log("socket on message");
       setMessageList([...messageList, {name, text}]);
     });
   })
@@ -25,18 +33,12 @@ function App() {
    <div className="App">
     <Container style={{paddingTop:"25px"}}>
       <Grid columns={2} divided id="Grid"
-        style={
-          {
-           backgroundColor:"blue",
-           height:"80vh",
-           borderRadius:"10px"
-          }
-          }>
+        style={gridStyles}>
         <Grid.Row stretched>
-          <Grid.Column style={{width:"30%"}}>
-              <RoomList />
+          <Grid.Column style={{width:"30%", maxHeight:"75vh"}}>
+              <UserList />
           </Grid.Column>
-          <Grid.Column style={{width:"70%"}}>
+          <Grid.Column style={{width:"70%",maxHeight:"75vh"}}>
             <MessageList messages={messageList}/>
             <SendMessageForm />
         </Grid.Column>
