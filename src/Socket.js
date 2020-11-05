987ac6a72547e851ee8ae1bd03053141e6497ec7
Chris <34993144+Mesiow@users.noreply.github.com>
2020-11-05 16:26:14 -0500

    fix

--- src/Socket.js
+++ src/Socket.js
@@ -1,6 +1,10 @@
 import io from 'socket.io-client';
 //need to be listening on port 4000(server)
 const local = "http://localhost:4000";
-const socket = io.connect(window.location.hostname);
+const hostname = window.location.hostname;
+console.log(hostname);
+var HOST = location.origin.replace(/^http/, 'ws')
+var ws = new WebSocket(HOST);
+const socket = io.connect("https://evening-ocean-58732.herokuapp.com/");
 
 export {socket};
