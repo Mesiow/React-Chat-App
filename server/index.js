75ae43c246cea9ec5a6a5f7763c60c38487da5b1
Chris <34993144+Mesiow@users.noreply.github.com>
2020-11-05 14:43:04 -0500

    add server disconnection event

--- server/index.js
+++ server/index.js
@@ -2,10 +2,23 @@ const app = require("express")();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
+var connectedClients = [];
+
 io.on("connection", socket => {
     //On user connected
     socket.on("connected", name => {
-        io.emit("connected", name); //emit name to all clients on connection
+        //store user's name in the socket session for this client
+        socket.name = name;
+        connectedClients.push({name: socket.name, id: socket.id});
+        //emit all current connected clients array to update list for new clients
+        io.emit("connected", (connectedClients)); 
+    });
+    //On user disconnect
+    socket.on("disconnect", () => {
+        console.log(socket.name + "disconnected");
+        //remove disconnected client from array
+        connectedClients = connectedClients.filter(client => client.id !== socket.id);
+        io.emit("disconnect", socket.name);
     });
     //On user message sent
     socket.on("message", ({name, text}) => { //message event
