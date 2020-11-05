75a85895981e5cf84d88b07d43fc90f7ce2cd436
Chris <34993144+Mesiow@users.noreply.github.com>
2020-11-05 16:11:54 -0500

    fix connection url

--- src/Socket.js
+++ src/Socket.js
@@ -1,5 +1,6 @@
 import io from 'socket.io-client';
 //need to be listening on port 4000(server)
-const socket = io.connect("http://localhost:4000");
+const local = "http://localhost:4000";
+const socket = io.connect(window.location.hostname);
 
 export {socket};
