6b4e079a0eb2b468664df65ee43f28fab3750c01
Chris <34993144+Mesiow@users.noreply.github.com>
2020-11-06 11:40:55 -0500

    change server url and fix packagejson

--- src/Socket.js
+++ src/Socket.js
@@ -1,8 +1,14 @@
 import io from 'socket.io-client';
 //need to be listening on port 4000(server)
-const local = "http://localhost:4000";
-const socketURL = "/";
+
+let server = window.location.hostname;
+const env = process.env.NODE_ENV;
+
+if(env === 'development'){
+    server = "http://localhost:4000";
+    console.log("using dev port");
+}
 const siteURL = "https://evening-ocean-58732.herokuapp.com/";
-const socket = io.connect();
+const socket = io.connect(server);
 
 export {socket};
