88742e6a40a624676e6a7da461bdb808cfd5b21c
Chris <34993144+Mesiow@users.noreply.github.com>
2020-11-06 11:54:16 -0500

    add build folder

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
+
+app.use(express.static(__dirname + '../build'));
 
 var connectedClients = [];
 
