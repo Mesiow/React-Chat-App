8c60c90ade6be36ebd8034e61def91db15c41e09
Chris <34993144+Mesiow@users.noreply.github.com>
2020-11-06 11:20:01 -0500

    add site url

--- src/Socket.js
+++ src/Socket.js
@@ -2,6 +2,7 @@ import io from 'socket.io-client';
 //need to be listening on port 4000(server)
 const local = "http://localhost:4000";
 const socketURL = "/";
-const socket = io.connect(socketURL);
+const siteURL = "https://evening-ocean-58732.herokuapp.com/";
+const socket = io(siteURL);
 
 export {socket};
